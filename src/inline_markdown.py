32545617c15804caa226c5766a156d77310fe39a
drewg73 <drew.garbenis@icloud.com>
2024-07-04 13:42:18 -1000

    3: fixed bug in inline_markdown.py

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -13,7 +13,7 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
 	new_nodes = []
 	for old_node in old_nodes:
 		if old_node.text_type != text_type_text:
-			new_nodes.append(old_Node)
+			new_nodes.append(old_node)
 		sections = old_node.text.split(delimiter)
 		if len(sections) %2 == 0:
 			raise ValueError("Invalid markdown, formatted section not closed")
