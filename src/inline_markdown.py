fa619e61d2e8e341eacc9f0d88cc380bc375293a
drewg73 <drew.garbenis@icloud.com>
2024-07-04 13:48:04 -1000

    4: add test_inline_markdown.py

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -14,6 +14,7 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
 	for old_node in old_nodes:
 		if old_node.text_type != text_type_text:
 			new_nodes.append(old_node)
+			continue
 		sections = old_node.text.split(delimiter)
 		if len(sections) %2 == 0:
 			raise ValueError("Invalid markdown, formatted section not closed")
