de7c459db3bdc47f541e591056daa4fefc83b444
Drew Garbenis <70610167+drewg73@users.noreply.github.com>
2024-07-06 10:59:44 -1000

    10: update test_block_markdown.py
    Added additional test cases.

--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -26,6 +26,30 @@ class TestMarkdownToBlock(unittest.TestCase):
          expected = ["wow, that's a lot of space"]
          actual = markdown_to_blocks(text)
          self.assertListEqual(actual, expected)
+	    
+    def test_consecutive_empty_lines(self):
+         text = "# This is a heading\n\n\nThis is a paragraph.\n\n\n* This is a list item"
+         expected = [
+              "# This is a heading",
+              "This is a paragraph.",
+              "* This is a list item"
+         ]
+         actual = markdown_to_blocks(text)
+         self.assertListEqual(actual, expected)
+
+    def test_empty_text(self):
+        text = ""
+        expected = []
+        actual = markdown_to_blocks(text)
+        self.assertListEqual(actual, expected)
+
+    def test_one_large_block(self):
+        text = "Line one\nLine two\nLine three\nLine four"
+        expected = [
+           "Line one\nLine two\nLine three\nLine four"
+        ]
+        actual = markdown_to_blocks(text)
+        self.assertListEqual(actual, expected)
 
 if __name__ == "__main__":
 	unittest.main()
